2676c76b9c1263c9850aa1cff1be58cb39f7d9dc
S-VIN <7530023@gmail.com>
2020-10-27 22:57:30 +0300

    normal get request

--- main.go
+++ main.go
@@ -1,79 +1,29 @@
-/*package main
-import (
-	"fmt"
-	"github.com/gocolly/colly")
-const CollyDocsLink = "https://baneks.ru/1141"
-
-func main() {
-	collector := colly.NewCollector()
-
-
-	collector.Visit(func(e *colly.HTMLElement){
-		println(e.Text)
-	})
-	collector.Visit(CollyDocsLink)
-	fmt.Print(collector.OnResponse(func(r *colly.Response) {
-		println(r)
-	}))
-}
-
-
 package main
 
 import (
 	"fmt"
 	"github.com/gocolly/colly"
-)
-
-const CollyDocsLink = "https://baneks.ru/1141"
-
-func main() {
-	for i := 1; i < 1141; i++ {
-		c := colly.NewCollector()
-		c.OnResponse(func(r *colly.Response) {
-			fmt.Println("Visited", r.Request.URL)
-			fmt.Println(r.Request.Headers)
-		})
-
-		c.Visit("https://baneks.ru/" + string(i))
-	}
-
-
-	for i := 1000; i < 1141; i++ {
-		c := colly.NewCollector()
-		c.OnResponse(func(r *colly.Response) {
-			fmt.Println("Visited", r.Request.URL)
-			fmt.Println(r.Request.Headers)
-		})
-
-		c.Visit("https://baneks.ru/" + string(i))
-	}
-
-}*/
-
-package main
-
-import (
-	"fmt"
-	"github.com/gocolly/colly"
+	"strconv"
 )
 
 func main() {
-	// Instantiate default collector
-	for i := 1; i < 1141; i++ {
-		c := colly.NewCollector()
 
-		// After making a request get "url" from
-		// the context of the request
-		c.OnResponse(func(r *colly.Response) {
-			fmt.Println("OK")
-		})
+	for i := 1; i < 1143; i++ {
+		c := colly.NewCollector(
+			// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+			colly.AllowedDomains("baneks.ru"),
+		)
 
+		// On every a element which has href attribute call callback
 		c.OnHTML("article", func(e *colly.HTMLElement) {
 			fmt.Println(e.Text)
 		})
 
-		c.Visit("https://baneks.ru/" + string(i))
-		c.Wait()
+		// Before making a request print "Visiting ..."
+		c.OnRequest(func(r *colly.Request) {
+
+		})
+
+		c.Visit("https://baneks.ru/" + strconv.Itoa(i))
 	}
 }
