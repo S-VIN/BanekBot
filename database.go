50d3844225b78bed5b94a1a17022e1f31a7e6aae
S-VIN <7530023@gmail.com>
2020-11-05 18:40:26 +0000

    do postgres

--- database.go
+++ database.go
@@ -4,25 +4,49 @@ import (
 	"context"
 	"fmt"
 	"os"
-
+	"github.com/gocolly/colly"
+	"strconv"
 	"github.com/jackc/pgx"
 )
 
+
+func getAnec(index int) string {
+	var output string
+	c := colly.NewCollector()
+	c.OnHTML("article", func(e *colly.HTMLElement) {
+		output = e.Text
+	})
+
+	c.Visit("https://baneks.ru/" + strconv.Itoa(index))
+	return output
+}
+
 func main() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("aneks"))
+	conn, err := pgx.Connect(context.Background(), "user=stepan password= host=localhost port=5432 dbname=stepan sslmode=verify-ca")
+	
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
+	
 	defer conn.Close(context.Background())
+	//for i:=-5; i < 500; i++{
+		//var text string
+		//var number int
+		//text = getAnec(i)
+		//_, err = conn.Exec(context.Background(), "INSERT INTO aneks(number, anek)VALUES ("+ strconv.Itoa(i) + ", '" + text + "');" )
+		 rows, _ := conn.Query(context.Background(), "select * from aneks")
 
-	var number int
-	var text string
-	err = conn.QueryRow(context.Background(), "select * from aneks").Scan(&number, &text)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(number, text)
+		 for rows.Next() {
+			 var number int
+			 var text string
+			 err := rows.Scan(&number, &text)
+			 if err == nil{
+				fmt.Printf("1")
+			 }
+			 fmt.Printf("%d. %s\n", number, text)
+		 }
+		// fmt.Println(number, text)
+	//}
 }
+
